1b77a978388bb834bff7a5d241e5c7645a7ac715
NICO <annicolas2@gmail.com>
2025-04-09 19:13:17 -0500

    Ejercicio

--- README.md
+++ README.md
@@ -9,4 +9,27 @@ Crear un programa en Javascript que realice lo siguiente:
 * Debe imprimir los números por consola o por el DOM ordenados de mayor a menor, y de menor a mayor.
 * Debe ser capaz de identificar si los números son iguales e imprimir un mensaje por consola o por el DOM diciendo que los números son iguales.
 
+Prueba tu programa con las siguientes entradas para asegurarte que funcione correctamente:
+
+Entradas: 
+4, 4, 2.
+
+Salida:
+4, 4, 2.
+2, 4, 4.
+
+Entradas: 
+4, 2, 4.
+
+Salida:
+4, 4, 2.
+2, 4, 4.
+
+Entradas: 
+2, 4, 4.
+
+Salida:
+4, 4, 2.
+2, 4, 4.
+
 Pruébalo con las combinaciones de números que se te ocurran.
--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 //Ejercicio Lógica 1
-// const prompt = require("prompt-sync")();
 
 document.getElementById("boton").addEventListener("click", () =>{
     event.preventDefault();
@@ -8,9 +7,35 @@ document.getElementById("boton").addEventListener("click", () =>{
     let numero2 = document.getElementById("segundo-numero").value;
     let numero3 = document.getElementById("tercer-numero").value;
 
-    let listaOrdenada = [];
+    let listaMayor = numerosMayorMenor(numero1, numero2, numero3);
+    let listaMenor = numerosMenorMayor(numero1, numero2, numero3);
 
-    if (){
-        
-    }
+    //Mostrar en consola
+    console.log(`${listaMayor}\n${listaMenor}`);
+    //Mostrar en documento
+    const texto = document.createElement('p');
+    texto.innerHTML = `${listaMayor}<br>${listaMenor}`;
+    document.getElementById("numeros").appendChild(texto);
 })
+
+function numerosMayorMenor(num1, num2, num3){
+    let listaOriginal = [num1, num2, num3];
+
+    if (num1 == num2 && num2 == num3){
+        return `Todos los números son iguales!`;
+    } else {
+        let listaOrdenada = [...listaOriginal].sort((a, b) => b - a); //Método para ordenar lista de números.
+        return `${listaOrdenada}`;
+    }
+};
+
+function numerosMenorMayor(num1, num2, num3){
+    let listaOriginal = [num1, num2, num3];
+
+    if (num1 == num2 && num2 == num3){
+        return `Todos los números son iguales!`;
+    } else {
+        let listaOrdenada = [...listaOriginal].sort((a, b) => a - b); //Método para ordenar lista de números.
+        return `${listaOrdenada}`;
+    }
+}
